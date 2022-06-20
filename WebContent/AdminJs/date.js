79c6939abf962eb2def870a6e3ffde6b9b4ee971
m-sagawa <m.shingaki@ks-japan.jp>
2022-06-20 10:21:32 +0900

    商品登録画面（管理者）修正２回目

--- WebContent/AdminJs/date.js
+++ WebContent/AdminJs/date.js
@@ -2,14 +2,14 @@
 
 
 /*生年月日のセレクトタグにオプション追加*/
-let item_id = document.getElementById("birthday_year");
-let item_id = document.getElementById("birthday_month");
-let item_id = document.getElementById("birthday_day");
+let birthday_year = document.getElementById("birthday_year");
+let birthday_month = document.getElementById("birthday_month");
+let birthday_day = document.getElementById("birthday_day");
 
 
 //1920年まで表示
 for (let i = year; i >= 1920; i--) {
- const option = document.createElement("option");
+	const option = document.createElement("option");
 	option.text = i;
 	option.value = i;
 	birthday_year.appendChild(option);
@@ -17,7 +17,7 @@ for (let i = year; i >= 1920; i--) {
 
 //1～12の数字を生成
 for (let i = 1; i <= 12; i++) {
-  const option = document.createElement("option");
+	const option = document.createElement("option");
 	option.text = i;
 	option.value = i;
 	birthday_month.appendChild(option);
@@ -25,7 +25,7 @@ for (let i = 1; i <= 12; i++) {
 
 //1～31の数字を生成
 for (let i = 1; i <= 31; i++) {
-  const option = document.createElement("option");
+	const option = document.createElement("option");
 	option.text = i;
 	option.value = i;
 	birthday_day.appendChild(option);
