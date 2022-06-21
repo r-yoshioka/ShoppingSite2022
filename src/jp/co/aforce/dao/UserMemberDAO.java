a9cfe0f0e23ca4aa1ae44a428f3131fd74fd1b6f
r.yoshioka@ks-japan.jp <r.yoshioka@ks-japan.jp>
2022-06-21 09:57:42 +0900

    登録画面修正（インデント）

--- src/jp/co/aforce/dao/UserMemberDAO.java
+++ src/jp/co/aforce/dao/UserMemberDAO.java
@@ -11,14 +11,14 @@ public class UserMemberDAO extends DAO {
 
 	public int searchReginst(UserReginstBean urb) throws Exception {
 
-        Connection con = getConnection();
+		Connection con = getConnection();
 
-        PreparedStatement st = con.prepareStatement(
-                "SELECT * FROM MEMBER_INFO_KSJ WHERE "
-                + " NAME =  ? AND SEX = ? AND BIRTH_YEAR = ? AND BIRTH_MONTH = ? AND BIRTH_DAY = ? "
-                + "AND JOB = ? AND PHONE_NUMBER = ? AND MAIL_ADDRESS = ? AND  PASSWORD = ?");
+		PreparedStatement st = con.prepareStatement(
+				"SELECT * FROM MEMBER_INFO_KSJ WHERE "
+						+ " NAME =  ? AND SEX = ? AND BIRTH_YEAR = ? AND BIRTH_MONTH = ? AND BIRTH_DAY = ? "
+						+ "AND JOB = ? AND PHONE_NUMBER = ? AND MAIL_ADDRESS = ? AND  PASSWORD = ?");
 
-        st.setString(1, urb.getName());
+		st.setString(1, urb.getName());
 		st.setString(2, urb.getSex());
 		st.setInt(3, urb.getBirthYear());
 		st.setInt(4, urb.getBirthMonth());
@@ -28,14 +28,14 @@ public class UserMemberDAO extends DAO {
 		st.setString(8, urb.getMailAddress());
 		st.setString(9, urb.getPassword());
 
-        ResultSet rs = st.executeQuery();
+		ResultSet rs = st.executeQuery();
 
-        int line = 0;
-        while (rs.next()) {
-        	line++;
-        }
-        return line;
-    }
+		int line = 0;
+		while (rs.next()) {
+			line++;
+		}
+		return line;
+	}
 
 	public int insert(UserReginstBean urb) throws Exception {
 
